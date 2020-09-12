bf820d9544a7f23361ac9f8a442d4283ee5f3320
Sidney 'PEPO' (aka sysb1n) <66956322+sidneypepo@users.noreply.github.com>
2020-09-12 04:44:45 +0000

    Add files via upload

--- .wwc-core/decrypt.sh
+++ .wwc-core/decrypt.sh
@@ -3,35 +3,117 @@
 #Var for input string:
 inp=$1
 
+#Number cutter function:
+cutn() 
+{
+	case $1 in 
+		17) echo -n "a" ;;
+		23) echo -n "b" ;;
+		5) echo -n "c" ;;
+		18) echo -n "d" ;;
+		20) echo -n "e" ;;
+		25) echo -n "f" ;;
+		21) echo -n "g" ;;
+		9) echo -n "h" ;;
+		15) echo -n "i" ;;
+		16) echo -n "j" ;;
+		1) echo -n "k" ;;
+		19) echo -n "l" ;;
+		4) echo -n "m" ;;
+		6) echo -n "n" ;;
+		7) echo -n "o" ;;
+		8) echo -n "p" ;;
+		10) echo -n "q" ;;
+		11) echo -n "r" ;;
+		12) echo -n "s" ;;
+		26) echo -n "t" ;;
+		24) echo -n "u" ;;
+		3) echo -n "v" ;;
+		22) echo -n "w" ;;
+		2) echo -n "x" ;;
+		14) echo -n "y" ;;
+		13) echo -n "z" ;;
+		*) echo -n "$1" ;;
+	esac
+}
+
+
 #Printing original string:
 echo -e "·Original string: ' $inp '"
 
-#Decrypt layer 1 - :
-a="$(echo ${inp//[^[:digit:]]/ })"
-dl1="$(for cntn in $(seq $(echo $a | wc -c))
+#Decrypt layer 1 - Separating all numbers and removing a random char after divided number:
+dl1="$(for cntn in $(seq $(echo $inp | wc -c))
 do
 
 	#Separating a number:
-	isn="$(echo $cntn | grep "[0-9]")"
+	isn="$(echo -n $inp | cut -b $cntn)"
 
-	if [[ "$isn" == [0-9] ]]
+	#Removing the random char after the number:
+	if [[ "$isn" != [0-9] ]]
 	then
 
-		let cnt++
-		echo -n $(($cntn/$cnt))
+		echo $inp | cut -b $cntn | tr -d a-z
 
 	else
 
-		echo -n $cntn
+		echo $isn | tr -d "\n" | tr "\n" " "
 
 	fi
 
 done)"
-#Decrypt layer 2 - :
+
+#Decrypt layer 2 - Dividing every single number by the position of the count of its respective column:
+dl2="$(for cntl in $dl1
+do
+
+	#Separating a number:
+	isl="$(echo $cntl | grep "[1-9]")"
+
+	#Dividing the number by the position of the count of its respective column
+	if [[ "$isl" != "" ]]
+	then
+
+		let cnt++
+		echo $(($cntl/$cnt))
+
+	else
+
+		echo $cntl
+
+	fi
+
+done)"
+
+#Encrypt layer 3 - Choosing the number in its respective line and converting it into an a-z char:
+dl3="$(for cntc in $dl2
+do
+
+	#Separating a number:
+	isl="$(echo $cntc | grep "[1-9]")"
+
+	#Converting number into char:
+	if [ "$isl" != "" ]
+	then
+
+		cutn $cntc
+
+	else
+
+		echo $cntc
+
+	fi
+
+done | tr -d " ")"
+
+#Decrypt layer 4 - Re-organizing text:
+dl4="$(echo $dl3 | tr -d " " | tr "\n" " ")"
+
+#Decrypt layer 5 - Removing final square bracket:
+dl5="$(echo $dl4 | tr -d "]")"
+
+#Decrypt layer 6 - Removing backslashes and switching it into blank spaces:
+dl6="$(echo $dl5 | tr "\\\\" " ")"
 
 #Printing final decrypted text:
-echo -e "·Decrypted text: ' $dl1 '\n"
+echo -e "·Decrypted text: ' $dl6 '\n"
 exit
-
-#echo -e "·This function is under construction...\n·Sorry for this inconvenient :/\n"
-#exit
--- .wwc-core/encrypt.sh
+++ .wwc-core/encrypt.sh
@@ -7,17 +7,20 @@ inp=$1
 cutt() { echo "a b c d e f g h i j k l m n o p q r s t u v w x y z" | cut -d "$1" -f 1 | tr " " "\n" | wc -l; }
 
 #Random letter chooser function:
-rl() { echo " m n b v c x z l k j h g f d s a p o i u y t r e w q" | cut -d " " -f $((($RANDOM%25)+1)); }
+rl() { echo "m n b v c x z l k j h g f d s a p o i u y t r e w q" | cut -d " " -f $((($RANDOM%25)+1)); }
 
 #Printing original text:
 echo -e "·Original text: ' $inp '"
 
 #Encrypt layer 1 - Switching alphabet:
 el1="$(echo $inp | tr "abcdefghijklmnopqrstuvwxyz" "qwertyuiopasdfghjklzxcvbnm")"
+
 #Encrypt layer 2 - Removing blank spaces and switching it into backslashes:
 el2="$(echo $el1 | tr " " "\\\\")"
+
 #Encrypt layer 3 - Removing final return key and switching it into a square bracket:
 el3="$(echo $el2 | tr "\n" "]")"
+
 #Encrypt layer 5 - Separating all chars with returns and switching all into numbers:
 el5="$(for cntc in $(seq $(echo -n $el3 | wc -c))
 do
@@ -40,7 +43,7 @@ do
 
 done | tr "\n" " ")"
 
-#Encrypt layer 6 - Separating all numbers and multiplying every single one by the position of the count of its respective column and adding a random char after multiplied number:
+#Encrypt layer 6 - Separating all numbers, multiplying every single one by the position of the count of its respective column and adding a random char after multiplied number:
 el6="$(for cntn in $el5
 do
 
@@ -63,5 +66,5 @@ do
 done)"
 
 #Printing final encrypted string:
-echo -e "·Encrypted string: ' $el6 '\n"
+echo -e "·Encrypted string: ' $el5 '\n"
 exit
